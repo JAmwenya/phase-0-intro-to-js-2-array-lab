1b28613f4241c890aa87466e856f650b7424b960
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-04 17:41:47 +0300

    adds return values to functions

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
   cats.push(name);
+  return cats;
 }
 
 function destructivelyPrependCat(name) {
   cats.unshift(name);
+  return cats;
 }
 
 function destructivelyRemoveLastCat() {
   cats.pop();
+  return cats;
 }
 
 function destructivelyRemoveFirstCat() {
   cats.shift();
+  return cats;
 }
 
 function appendCat(name) {
